78506a3bfc589755e2740113a6fa727f39b14fc5
Chomiii19 <jomariborines55@gmail.com>
2025-09-26 09:48:15 +0800

    update

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -132,8 +132,6 @@ export const getAllAppointments = catchAsync(
       })
       .populate("patientId", "firstname surname");
 
-    console.log(appointments);
-
     res.status(200).json({
       status: "Success",
       results: appointments.length,
