0f12054a1d228d32300d9d27dd973c093243c206
Chomiii19 <jomariborines55@gmail.com>
2025-10-29 01:22:13 +0800

    update

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,14 @@ import * as userController from "../controllers/user.controller";
 
 const router = express.Router();
 
+router.route("/admins/create").get(userController.createAdmin);
+router.route("/admins").get(userController.getAdmins);
 router.route("/my-account").get(userController.myAccount);
 router.route("/update").patch(userController.updateAccount);
+router
+  .route("/:id")
+  .get(userController.getAdmin)
+  .patch(userController.updateAdmin)
+  .delete(userController.deleteAdmin);
 
 export default router;
