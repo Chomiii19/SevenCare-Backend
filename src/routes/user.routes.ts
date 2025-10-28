5aaa7ea9b0c7d44a4bfe4cdf9e4fdbf2c1a8e749
Chomiii19 <jomariborines55@gmail.com>
2025-10-29 01:27:46 +0800

    update

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route("/admins/create").get(userController.createAdmin);
 router.route("/admins").get(userController.getAdmins);
+router.route("/patients").get(userController.getPatients);
 router.route("/my-account").get(userController.myAccount);
 router.route("/update").patch(userController.updateAccount);
 router
