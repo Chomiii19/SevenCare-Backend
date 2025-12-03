05fa16abcc6588f3be3c3099e287a02576ea229f
Chomiii19 <jomariborines55@gmail.com>
2025-12-03 18:27:55 +0800

    update

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -191,7 +191,7 @@ export const getAllAppointments = catchAsync(
     const { status, date, service } = req.query;
 
     const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 10;
+    const limit = parseInt(req.query.limit as string) || 15;
     const skip = (page - 1) * limit;
 
     const filter: any = { isDeleted: false };
