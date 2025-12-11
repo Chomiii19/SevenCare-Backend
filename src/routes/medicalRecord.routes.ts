ecc624fd20ef07cd948707643a9c9c1303b4860a
Chomiii19 <jomariborines55@gmail.com>
2025-12-11 21:55:49 +0800

    update

--- src/routes/medicalRecord.routes.ts
+++ src/routes/medicalRecord.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { upload } from "../middlewares/multer";
-import { uploadMedicalRecord } from "../controllers/medicalRecord.controller";
+import {
+  deleteMedicalRecord,
+  uploadMedicalRecord,
+} from "../controllers/medicalRecord.controller";
 
 const router = express.Router();
 
 router.post("/upload", upload.single("file"), uploadMedicalRecord);
+router.delete("/:id", deleteMedicalRecord);
 
 export default router;
