fac6b49ceed0dd3aa518acf3374651d2208fc0b4
Chomiii19 <jomariborines55@gmail.com>
2026-02-01 13:16:31 +0800

    update

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -12,7 +12,7 @@ router.route("/admins").get(userController.getAdmins);
 router.route("/patients").get(userController.getPatients);
 router.route("/my-account").get(userController.myAccount);
 router.route("/update").patch(userController.updateAccount);
-router.patch("/users/:id/password", userController.changePassword);
+router.patch("/:id/password", userController.changePassword);
 router
   .route("/:id")
   .get(userController.getAccount)
